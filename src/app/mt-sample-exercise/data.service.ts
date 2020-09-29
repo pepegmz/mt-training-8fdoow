69b0b9b3ab01d60e2e19e68b8dae99dd90825484
pepegmz <pepe.gmz.404@gmail.com>
2020-09-28 21:15:59 -0500

    grid load async data

--- src/app/mt-sample-exercise/data.service.ts
+++ src/app/mt-sample-exercise/data.service.ts
@@ -1,4 +1,16 @@
+import { Observable } from 'rxjs'
+import * as data from './mock-data.json';
+import { map } from 'rxjs/operators';
+
 export class DataService {
-    
+    farms$: Observable<any>;
+
+    getFarms() {
+      return this.farms$ = new Observable((subs) => {
+        subs.next(data);
+      }).pipe(
+          map((value) => value)
+      );
+    }
 }
 
--- src/app/mt-sample-exercise/mt-sample-list-index.component.ts
+++ src/app/mt-sample-exercise/mt-sample-list-index.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
 import { SelectedFarmService } from './selected-farm.service';
 import { Observable, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, map } from 'rxjs/operators';
 import { Farm } from './farm';
 
 @Component({
   selector: 'mt-sample-list',
   templateUrl: './mt-sample-list-index.component.html'
 })
-export class MtSampleListIndexComponent {
+export class MtSampleListIndexComponent implements OnInit {
 
-  constructor() {
+   public pageSettings: any;
+
+  constructor(public dataService: DataService) {
 
   }
+
+  ngOnInit(): void {
+    this.dataService.getFarms();
+    this.pageSettings = { pageSize: 6 };
+  }
+
+
 
 }
